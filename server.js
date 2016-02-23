b394ec31d3a286b5372a84c8a3ca34f348a54335
Luke Madera <luke.madera@gmail.com>
2016-02-22 21:41:27 -0800

    chore: support heroku port.

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, function() {
+app.listen((process.env.PORT || 3000), function() {
   console.log("Listening on port 3000");
 });
